189d3edc71276e6de1e33907e3e6768e5791c790
Yousof27 <yousofkamal111@gmail.com>
2025-10-17 01:33:13 +0300

    Update Project Domain

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,6 @@
 export const PAGE_LIMIT = 6;
-export const DOMAIN = "http://localhost:3000";
+
+const developmentDomain = "http://localhost:3000";
+const productionDomain = "https://cloudhosting01.netlify.app";
+
+export const DOMAIN = process.env.NODE_ENV === "production" ? productionDomain : developmentDomain;
